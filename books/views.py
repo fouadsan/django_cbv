5c2cc689e5c37b8e00a9166fa846162447a4dd82
fouadsan <F.benayad95@gmail.com>
2021-11-05 07:42:29 +0100

    DetailView

--- books/views.py
+++ books/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from  django.views.generic.detail import DetailView
+from django.db.models import F
+from django.utils import timezone
 
 from .models import Book
 
 
 class IndexView(TemplateView):
 
-    template_name = "home.html"
+    template_name = "books/home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -14,11 +17,18 @@ class IndexView(TemplateView):
         return context
 
 
-class BookDetailView(TemplateView):
+class BookDetailView(DetailView):
 
-    template_name = "home.html"
+    model = Book
+    
+    # Additional stuff:
+    
+    template_name = "books/book-detail.html"  # must include it if not the same name 
+    context_object_name = 'book' # in html {{book.title}}
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['books'] = Book.objects.all()
+        book = Book.objects.filter(slug=self.kwargs.get('slug'))
+        book.update(count=F('count') + 1)
+        context['time'] = timezone.now()
         return context
