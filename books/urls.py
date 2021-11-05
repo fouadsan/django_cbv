627551ad69ec58a523ea01835c20107caff979d2
fouadsan <F.benayad95@gmail.com>
2021-11-05 08:33:29 +0100

    ListView

--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import IndexView, BookDetailView
+from .views import IndexView, BookDetailView, GenreView
 
 app_name = 'books'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('g/<str:genre>', GenreView.as_view(), name='genre'),
     path('<slug:slug>/', BookDetailView.as_view(), name='book-detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,30 +1,46 @@
+from typing import List
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from  django.views.generic.detail import DetailView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from django.db.models import F
 from django.utils import timezone
 
 from .models import Book
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
 
+    model = Book
     template_name = "books/home.html"
+    context_object_name = "books"  # default = "object_list"
+    paginate_by = 2
+    # queryset = Book.objects.all()[:2]
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['books'] = Book.objects.all()
-        return context
+    def get_queryset(self):
+        return Book.objects.all()[:2]
+
+
+class GenreView(ListView):
+
+    model = Book
+    template_name = "books/home.html"
+    context_object_name = "books"  # default = "object_list"
+    paginate_by = 2
+
+    def get_queryset(self):
+        return Book.objects.filter(genre__icontains=self.kwargs.get('genre'))
 
 
 class BookDetailView(DetailView):
 
     model = Book
-    
+
     # Additional stuff:
-    
-    template_name = "books/book-detail.html"  # must include it if not the same name 
-    context_object_name = 'book' # in html {{book.title}}
+
+    # must include it if not the same name  default = "book_detail.html"
+    template_name = "books/book-detail.html"
+    context_object_name = 'book'  # in html {{book.title}}
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
